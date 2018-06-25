ce264b1d6f5a09e35d052b2fea6147bfc5612baa
PatriciaDurandPUCRS <patricia.durand@acad.pcrs.br>
2018-06-25 00:34:41 -0300

    Ajustando o login

--- src/api/login.js
+++ src/api/login.js
@@ -1,5 +1,5 @@
 import angular from 'angular';
 
 angular.module('app')
-  .constant('LOGIN_URL', '/teste/{0}')
+  .constant('LOGIN_URL', '/authenticate/')
   .constant('TESTE_DOIS_URL', '/teste/{0}/{1}');
