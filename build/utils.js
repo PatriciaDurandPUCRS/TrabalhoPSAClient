6005dd84d4ab60475011fbb515df6d0662f6e468
patricia.durand <patricia.durand@agiplan.com>
2018-06-24 23:27:54 -0300

    .

--- build/utils.js
+++ build/utils.js
@@ -5,7 +5,9 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const packageConfig = require('../package.json')
 
 exports.assetsPath = function (_path) {
-  const assetsSubDirectory = config.dev.assetsSubDirectory
+  const assetsSubDirectory = process.env.NODE_ENV === 'production'
+    ? config.build.assetsSubDirectory
+    : config.dev.assetsSubDirectory
 
   return path.posix.join(assetsSubDirectory, _path)
 }
--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -1,12 +1,15 @@
 'use strict'
 const utils = require('./utils')
 const config = require('../config')
-const sourceMapEnabled = config.dev.cssSourceMap
+const isProduction = process.env.NODE_ENV === 'production'
+const sourceMapEnabled = isProduction
+  ? config.build.productionSourceMap
+  : config.dev.cssSourceMap
 
 module.exports = {
   loaders: utils.cssLoaders({
     sourceMap: sourceMapEnabled,
-    extract: true
+    extract: isProduction
   }),
   cssSourceMap: sourceMapEnabled,
   cacheBusting: config.dev.cacheBusting,
